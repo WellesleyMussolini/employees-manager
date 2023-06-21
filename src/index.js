c79f1912cba9b4d4a14318989a9b15091eb5909a
wellesleymussolini <iei.muss@gmail.com>
2023-06-21 19:52:15 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,51 @@
+// import app from "./app.js";
+// import ip from "ip";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// const protocol = process.env.PROTOCOL || "http";
+// const address = ip.address();
+// const port = process.env.PORT || 3000;
+
+// app.listen(port, () => {
+//     console.log("Compiled successfully!");
+//     console.log("You can now view your app in the browser.");
+//     console.log(`Local:            "http://localhost:${port}"`);
+//     console.log(`On Your Network:  "${protocol}://${address}:${port}"`);
+// });
+
 import app from "./app.js";
 import ip from "ip";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const protocol = process.env.PROTOCOL || "http";
-const address = ip.address();
-const port = process.env.PORT || 3000;
+const startServer = async () => {
+    const protocol = process.env.PROTOCOL || "http";
+    const address = ip.address();
+    const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
-app.listen(port, () => {
+    await new Promise((resolve, reject) => {
+        app.listen({
+            host: '0.0.0.0',
+            port: port,
+        }, (err) => {
+            if (err) {
+                reject(err);
+            } else {
+                console.log("HTTP Server Running");
+                resolve();
+            }
+        });
+    });
+
     console.log("Compiled successfully!");
     console.log("You can now view your app in the browser.");
     console.log(`Local:            "http://localhost:${port}"`);
     console.log(`On Your Network:  "${protocol}://${address}:${port}"`);
+};
+
+startServer().catch((error) => {
+    console.error("Failed to start server:", error);
 });
